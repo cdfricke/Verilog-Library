8e34e311128eb62b5e6e9ac80f450bce2882876b
Connor Fricke <cd.fricke23@gmail.com>
2024-08-13 11:40:20 -0400

    complete goertzel_IIR v1

--- DSP/Filters/goertzel_IIR.v
+++ DSP/Filters/goertzel_IIR.v
@@ -1,22 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company: the Ohio State University
+// Engineer: Connor Fricke (cd.fricke23@gmail.com)
 // Create Date: 07/30/2024 12:07:03 PM
-// Design Name: 
 // Module Name: goertzel_IIR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
+// Project Name: Goertzel Simulation
+//
 // Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Fixed phase second-stage IIR Goertzel filter, cascade form. Phase is fixed with 
+//      k/N = 1/6 -> 2*PI*k/N = PI/3 -> cos() = 1/2, sin() = ~0.866
+// The difference equation for the IIR portion is given by:
+//      s(n) = x(n) + 2cos()*s(n-1) - s(n-2),  where cos() = 1/2
+//      s(n) = x(n) + s(n-1) - s(n-2)
+// The DFT coefficient for k/N = 1/6 is then given by an algebraic simplification of
+// the FIR portion of the filter:
+//      X(k) = cos()*s(N-1) - s(N-2) + i*sin()*s(N-1)
 //////////////////////////////////////////////////////////////////////////////////
 
 module goertzel_IIR
@@ -26,18 +24,21 @@ module goertzel_IIR
     parameter OW = 32
 )
 (
-    input i_clk, i_rst,
-    input signed [(IW-1):0]         i_sample
+    input                   i_clk, i_rst,
+    input signed [(IW-1):0] i_sample,       // Input signal, A(1,10) format
+    output reg              o_data_valid,   // 1 clk cycle pulse each time results are latched to output regs
+    output wire [(OW-1):0]  o_result_re,    // Re{X(k)}, A(1,10) format
+    output wire [(OW-1):0]  o_result_im     // Im{X(k)}, A(1,10) format
 );
 
-    // * OUTPUT MEM BLOCK *
+    // * INITIALIZE DELAY REG MEMORY BLOCK *
     reg signed [(OW-1):0] d_mem [1:0];
     initial begin
         d_mem[0] = 0;
         d_mem[1] = 0;
     end
 
-    // * COUNT SAMPLES *
+    // * COUNT SAMPLE # *
     localparam NW = $clog2(N);
     reg [(NW-1):0] n = 0;
 
@@ -46,7 +47,7 @@ module goertzel_IIR
         else n <= n + 1;
     end
 
-    // * PERFORM ADDITIONS *
+    // * COMBINATORIAL LOGIC STAGE, i.e. CALCULATE s[n] *
     // The standard difference equation for the IIR portion is 
     //  s(n) = x(n) + 2cos()s(n-1) - s(n-2)
     // however, we impose the constraint that k/N = 1/6 and thus cos(2pik/N) = cos(pi/3) = 1/2
@@ -66,19 +67,34 @@ module goertzel_IIR
         end
     end
 
-    // s[N], s[N-1], s[N-2] for final calculation
+    // * CALCULATE s[N-1], s[N-2] *
+    // s[N-1], s[N-2] used for final calculation of X(k)
     reg signed [(OW-1):0] s [(N-1):(N-2)];
-
     initial begin
         s[N-1] = 0;
         s[N-2] = 0;
     end
-
     always @ (posedge i_clk) begin
         if ( n == (N-1) ) begin
             s[N-1] <= sum;
             s[N-2] <= d_mem[0];
         end
+    end
+
+    // * CALCULATE FINAL RESULTS, Re{X(k)}, Im{X(k)} *
+    // Re{X(k)} = cos()*s[N-1] - s[N-2] = 0.5*s[N-1] - s[N-2]
+    // Im{X(k)} = sin()*s[N-1]
+    // given k/N = 1/6, 2*PI*k/N = PI/3, cos(PI/3) = 1/2, sin(PI/3) = ~0.866
+    reg signed [7:0] SIN = 8'b01101111;     // = 111, or 0.8671875, A(0,7) format
+    assign o_result_re = (s[N-1] >>> 1) - s[N-2];
+    wire signed [(OW+7):0] mult = (SIN * s[N-1]) >>> 7;     // A(0,7) * A(a,b) = A(a+1, b+7) >>> 7 
+    assign o_result_im = mult[(OW-1):0];  
+
+    // * CONTROL DATA_VALID FLAG *
+    initial o_data_valid <= 1'b0;
+    always @ (posedge i_clk) begin
+        if ( n == (N-1) ) o_data_valid <= 1'b1;    
+        else o_data_valid <= 1'b0;
     end
 
 endmodule
--- DSP/goertzel_tb.v
+++ DSP/goertzel_tb.v
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 `define CLK_PER 10
+`define SIM     // must be either SYNTH or SIM
 //////////////////////////////////////////////////////////////////////////////////
 // Company: the Ohio State University
 // Engineer: Connor Fricke
@@ -13,41 +14,61 @@
 // Dependencies: 
 //  CORDIC IP, Sincos Module, Goertzel Module
 //////////////////////////////////////////////////////////////////////////////////
+// Current Results:
+// t_init   Re      Im          abs()
+// 210      -54.55  -31.55      63.02
+// 220      0.01    -63.08      63.08
+// 230      54.56   -31.53      63.02
+// 240      54.55   31.55       63.02
+// 250      -0.01   63.08       63.08
+// 260      -54.56  31.53       63.02
+// 270      -54.55  -31.55      63.02
 
+module goertzel_tb(
+    `ifdef SYNTH
+        input clk,
+        input rst,
+        output led
+    `endif
+);
 
-module goertzel_tb();
-
-    reg rst = 1'b1;
-    initial rst = #380 1'b0;
-    reg clk = 1'b0;
-    always clk = #(`CLK_PER/2) ~clk;
-    
+    `ifdef SIM
+        reg rst = 1'b1;
+        initial rst = #270 1'b0;
+        reg clk = 1'b0;
+        always clk = #(`CLK_PER/2) ~clk;
+    `endif
 
     // * DEFAULTS FOR GOERTZEL
     localparam SW = 12;             // signed signal width
-    localparam N = 60;              // y[N] = X(k) (the result we want)
-    localparam KW = $clog2(N);      // signed k width, needs to range from 0 to N/2, log2(N) bits gets us that
-    localparam PIW = 15;            // width of unsigned constant PI localparam
-    localparam PW = PIW+KW;         // phase 2*PI*k/N width
-    localparam CW = 16;             // CORDIC output width
-    localparam OW = 16;             // width of output and output delay registers
+    localparam N = 126;             // y[N] = X(k) (the result we want)
+    localparam OW = 18;             // width of Goertzel result
 
     // ** FEED SIGNAL **
-    // output signal is A(1,f) format as required by the CORDIC IP
+    // output signal is A(1,f) format, so f = SW-2
     wire signed [(SW-1):0] signal;
-    wave_gen_CORDIC #(.OW(SW), .PW(16)) freq_5MHz(
-        .clk(clk),
-        .rst(1'b0),
-        .i_phi(16'd8579),
-        .cos(),
-        .sin(signal)
+    wave_gen_CORDIC #(.OW(SW), .PW(16)) freq(
+        .clk    (clk),      // input clock
+        .rst    (1'b0),     // not used
+        .i_phi  (16'd8579), // phase step value
+        .cos    (),         // not used
+        .sin    (signal)    // sine wave result, A(1,f)
     );
 
-    goertzel_IIR goertzelAlgo(
-        .i_clk      (clk),
-        .i_rst      (rst),
-        .i_sample   (signal)
+    wire cycle_complete;
+    wire [(OW-1):0] Xk_re, Xk_im;
+
+    goertzel_IIR #(.IW(SW), .N(N), .OW(OW)) goertzelAlgo (
+        .i_clk          (clk),              // input clock
+        .i_rst          (rst),              // synch reset
+        .i_sample       (signal),           // Input signal, A(1,f) format
+        .o_data_valid   (cycle_complete),   // Active-High output flag signaling result has been calculated
+        .o_result_re    (Xk_re),            // Re{X(k)}, A(n,f) format
+        .o_result_im    (Xk_im)             // Im{X(k)}, A(n,f) format
     );
 
+    `ifdef SYNTH
+        assign led = cycle_complete;
+    `endif
 
 endmodule
